a60f4293303cffecf51414e3ceb68f23d883d50a
Sng Hao Jun <51106107+haojunsng@users.noreply.github.com>
2025-07-07 20:20:35 +0800

    Feature/fastapi security (#6)

--- climatact/main.py
+++ climatact/main.py
@@ -1,11 +1,29 @@
-from fastapi import FastAPI
+import os
+from fastapi import FastAPI, HTTPException, Security, Depends, Request
+from fastapi.security.api_key import APIKeyHeader
 from climatact.services.weather_service import get_weather_metrics
 from climatact.models.models import WeatherResponse
+from climatact.middleware.setup import setup_middlewares
 
-app = FastAPI()
+API_KEY = os.environ.get("API_KEY")
+API_KEY_NAME = "X-API-KEY"
+api_key_header = APIKeyHeader(name=API_KEY_NAME, auto_error=False)
 
+def create_app() -> FastAPI:
+    app = FastAPI()
 
-@app.get("/weather", response_model=WeatherResponse)
-async def get_weather(town: str):
-    response = await get_weather_metrics(town)
-    return response
+    setup_middlewares(app)
+
+    async def get_api_key(api_key: str = Security(api_key_header)):
+        if api_key != API_KEY:
+            raise HTTPException(status_code=401, detail="Invalid or missing API Key")
+
+    @app.get("/weather", response_model=WeatherResponse, dependencies=[Depends(get_api_key)])
+    @app.state.limiter.limit("10/minute")
+    async def get_weather(town: str, request: Request):
+        response = await get_weather_metrics(town)
+        return response
+
+    return app
+
+app = create_app()
