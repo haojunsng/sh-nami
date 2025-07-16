6ff8eb2852354609a981a2e2f1c07fe62ea3ce74
Sng Hao Jun <51106107+haojunsng@users.noreply.github.com>
2025-07-16 22:38:22 +0800

    feat: Add Openmeteo as 3rd weather source (#14)

--- climatact/services/weather_service.py
+++ climatact/services/weather_service.py
@@ -18,6 +18,7 @@ async def get_weather_metrics(town: str) -> dict:
 
     tomorrow_url = f"https://api.tomorrow.io/v4/weather/realtime?location={lat},{lon}&apikey={TOMORROW_API_KEY}"
     google_url = f"https://weather.googleapis.com/v1/currentConditions:lookup?key={GOOGLE_API_KEY}&location.latitude={lat}&location.longitude={lon}"
+    openmeteo_url = f"https://api.open-meteo.com/v1/forecast?latitude={lat}&longitude={lon}&current_weather=true"
 
     headers = {
         "accept": "application/json",
@@ -27,9 +28,11 @@ async def get_weather_metrics(town: str) -> dict:
         async with httpx.AsyncClient() as client:
             tomorrow_response = await client.get(tomorrow_url, headers=headers)
             google_response = await client.get(google_url)
+            openmeteo_response = await client.get(openmeteo_url)
 
         tomorrow_response.raise_for_status()
         google_response.raise_for_status()
+        openmeteo_response.raise_for_status()
     
     except httpx.HTTPStatusError as e:
         raise HTTPException(status_code=502, detail=f"Upstream service returned error: {e.response.status_code}")
@@ -38,4 +41,4 @@ async def get_weather_metrics(town: str) -> dict:
     except Exception as e:
         raise HTTPException(status_code=500, detail="Unexpected error occurred.")
 
-    return handler(town, tomorrow_response, google_response)
+    return handler(town, tomorrow_response, google_response, openmeteo_response)
